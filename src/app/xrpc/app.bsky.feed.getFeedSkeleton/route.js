0d85fc5f798c4a67f53db93c282adc4c8f82514a
andrew sòlanto <atsolanto@gmail.com>
2025-01-07 14:57:19 -0500

    remove feed name calculation

--- src/app/xrpc/app.bsky.feed.getFeedSkeleton/route.js
+++ src/app/xrpc/app.bsky.feed.getFeedSkeleton/route.js
@@ -3,11 +3,6 @@ import escape from "escape-string-regexp"
 import { AtpAgent } from "@atproto/api"
 
 export async function GET({ nextUrl: { searchParams } }) {
-	const feedMatch = "at://${did}/app.bsky.feed.generator/default"
-		.split("/")
-		.findLast(() => true)
-
-	const feed = feedMatch[1]
 	const cursor = searchParams.get("cursor")
 
 	const agent = new AtpAgent({ service: "https://bsky.social" })
@@ -17,18 +12,12 @@ export async function GET({ nextUrl: { searchParams } }) {
 		password: process.env.BSKY_PASSWORD
 	})
 
-	if (feed == "default")
-		return Response.json(
-			(
-				await agent.getAuthorFeed({
-					actor: "bsky.app",
-					cursor
-				})
-			).data
-		)
-
-	return new Response(null, {
-		status: 400,
-		statusText: "this feed doesn't exist"
-	})
+	return Response.json(
+		(
+			await agent.getAuthorFeed({
+				actor: "bsky.app",
+				cursor
+			})
+		).data
+	)
 }
