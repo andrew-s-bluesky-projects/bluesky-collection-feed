d46394f34aeb6185b536e6777f29b16df3904fec
andrew sòlanto <atsolanto@gmail.com>
2025-01-07 14:55:41 -0500

    fix feed name calculation

--- src/app/xrpc/app.bsky.feed.getFeedSkeleton/route.js
+++ src/app/xrpc/app.bsky.feed.getFeedSkeleton/route.js
@@ -3,35 +3,29 @@ import escape from "escape-string-regexp"
 import { AtpAgent } from "@atproto/api"
 
 export async function GET({ nextUrl: { searchParams } }) {
-	const feedMatch = searchParams
-		.get("feed")
-		.match(
-			new RegExp(
-				`^(?:${escape(`at://${did}/app.bsky.feed.generator/`)})(.*)$`
-			)
+	const feedMatch = "at://${did}/app.bsky.feed.generator/default"
+		.split("/")
+		.findLast(() => true)
+
+	const feed = feedMatch[1]
+	const cursor = searchParams.get("cursor")
+
+	const agent = new AtpAgent({ service: "https://bsky.social" })
+
+	await agent.login({
+		identifier: process.env.BSKY_HANDLE,
+		password: process.env.BSKY_PASSWORD
+	})
+
+	if (feed == "default")
+		return Response.json(
+			(
+				await agent.getAuthorFeed({
+					actor: "bsky.app",
+					cursor
+				})
+			).data
 		)
-
-	if (feedMatch !== null) {
-		const feed = feedMatch[1]
-		const cursor = searchParams.get("cursor")
-
-		const agent = new AtpAgent({ service: "https://bsky.social" })
-
-		await agent.login({
-			identifier: process.env.BSKY_HANDLE,
-			password: process.env.BSKY_PASSWORD
-		})
-
-		if (true)
-			return Response.json(
-				(
-					await agent.getAuthorFeed({
-						actor: "bsky.app",
-						cursor
-					})
-				).data
-			)
-	}
 
 	return new Response(null, {
 		status: 400,
